18ed97ed3fbdd69a2d1091f2541a1c9b699026b1
Roman Savenko <rsaven.wk@gmail.com>
2024-09-23 15:56:19 +0300

    feat: add user model

---
+++ FakeUserGenerator/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace FakeUserGenerator.Models;
+
+public class User
+{
+    public int Number { get; set; }
+    public Guid Id { get; set; }
+    public string? FullName { get; set; }
+    public string? Address { get; set; }
+    public string? PhoneNumber { get; set; }
+}
